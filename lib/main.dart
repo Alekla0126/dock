d137ed430f8ec81b81c61db8414af3018a579eae
alekla0126 <alekla0126@gmail.com>
2024-11-14 02:28:27 +0300

    1.0.2

--- lib/main.dart
+++ lib/main.dart
@@ -37,63 +37,162 @@ class _MacOSDockState extends State<MacOSDock> {
     DockItem(icon: Icons.photo),
   ];
 
-  int? hoveredIndex; // Track the index of the hovered item for zoom effect
-
-  void _reorderItems(int oldIndex, int newIndex) {
-    setState(() {
-      final item = items.removeAt(oldIndex);
-      items.insert(newIndex, item);
-    });
-  }
+  DockItem? draggedItem;
+  int? draggedItemIndex;
+  int? insertionIndex;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: Colors.grey[850],
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: items.asMap().entries.map((entry) {
-          int index = entry.key;
-          DockItem item = entry.value;
+    double dockWidth = MediaQuery.of(context).size.width * 0.8;
 
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: DragTarget<int>(
-              onAcceptWithDetails: (details) {
-                _reorderItems(details.data, index);
+    List<Widget> dockItems = [];
+    for (int i = 0; i <= items.length; i++) {
+      // Only add the DragTarget if it's a different position
+      if (i != draggedItemIndex) {
+        dockItems.add(
+          DragTarget<DockItem>(
+            onWillAccept: (data) {
+              if (insertionIndex != i) {
+                // Only update if the index has changed
+                setState(() {
+                  insertionIndex = i;
+                });
+              }
+              return true;
+            },
+            onLeave: (data) {
+              setState(() {
+                insertionIndex = null;
+              });
+            },
+            onAccept: (data) {
+              setState(() {
+                if (draggedItem != null && draggedItemIndex != null) {
+                  items.removeAt(draggedItemIndex!);
+                  int newIndex = i;
+                  if (i > draggedItemIndex!) {
+                    newIndex = i - 1;
+                  }
+                  items.insert(newIndex, draggedItem!);
+                }
+                draggedItem = null;
+                draggedItemIndex = null;
+                insertionIndex = null;
+              });
+            },
+            builder: (context, List<DockItem?> candidateData, rejectedData) {
+              bool isActive = candidateData.isNotEmpty && insertionIndex == i;
+              return AnimatedContainer(
+                duration: const Duration(milliseconds: 200),
+                width: isActive ? 80 : 64,
+                height: isActive ? 80 : 64,
+                alignment: Alignment.center,
+                child: isActive && draggedItem != null
+                    ? DockIcon(
+                        item: draggedItem!,
+                        isHovered: false,
+                        isDragging: false,
+                      )
+                    : const SizedBox(
+                        width: 64,
+                        height: 64,
+                      ),
+              );
+            },
+          ),
+        );
+      }
+
+      if (i < items.length) {
+        DockItem item = items[i];
+        dockItems.add(
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4.0),
+            child: MouseRegion(
+              key: ValueKey(item),
+              onEnter: (_) {
+                if (draggedItem == null) return;
+                setState(() {
+                  insertionIndex = i;
+                });
               },
-              builder: (context, candidateData, rejectedData) {
-                return AnimatedSwitcher(
-                  duration: const Duration(milliseconds: 300),
-                  transitionBuilder:
-                      (Widget child, Animation<double> animation) {
-                    return ScaleTransition(scale: animation, child: child);
-                  },
-                  child: MouseRegion(
-                    key: ValueKey(item),
-                    onEnter: (_) => setState(() => hoveredIndex = index),
-                    onExit: (_) => setState(() => hoveredIndex = null),
-                    child: DraggableDockItem(
-                      item: item,
-                      onDelete: () {
-                        setState(() {
-                          items.remove(item);
-                        });
-                      },
-                      index: index,
-                      isHovered: hoveredIndex == index,
-                    ),
-                  ),
-                );
+              onExit: (_) {
+                if (draggedItem == null) return;
+                setState(() {
+                  insertionIndex = null;
+                });
               },
+              child: DraggableDockItem(
+                item: item,
+                index: i,
+                onDragStarted: () {
+                  setState(() {
+                    draggedItem = item;
+                    draggedItemIndex = i;
+                  });
+                },
+                onDragEnd: (wasAccepted) {
+                  setState(() {
+                    draggedItem = null;
+                    draggedItemIndex = null;
+                    insertionIndex = null;
+                  });
+                },
+              ),
             ),
-          );
-        }).toList(),
-      ),
+          ),
+        );
+      }
+    }
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        // "Delete zone" adjusted to match the dock's width
+        DragTarget<DockItem>(
+          onWillAccept: (data) => true,
+          onAccept: (data) {
+            setState(() {
+              items.remove(data);
+            });
+          },
+          builder: (context, candidateData, rejectedData) {
+            return Container(
+              height: 80,
+              width: dockWidth,
+              decoration: BoxDecoration(
+                color: Colors.transparent, // Transparent background
+                border:
+                    Border.all(color: Colors.white, width: 2), // White border
+                borderRadius:
+                    BorderRadius.circular(8), // Rounded corners if desired
+              ),
+              child: const Center(
+                child: Text(
+                  'Drag here to delete',
+                  style: TextStyle(color: Colors.white), // White text
+                ),
+              ),
+            );
+          },
+        ),
+        const SizedBox(height: 10),
+        Container(
+          padding: const EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            color: Colors.grey[850],
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: SizedBox(
+            width: dockWidth,
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: dockItems,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
@@ -104,84 +203,88 @@ class DockItem {
   DockItem({required this.icon});
 }
 
+class DockIcon extends StatelessWidget {
+  final DockItem item;
+  final bool isHovered;
+  final bool isDragging;
+
+  const DockIcon({
+    Key? key,
+    required this.item,
+    this.isHovered = false,
+    this.isDragging = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      width: (isDragging || isHovered) ? 80 : 64,
+      height: (isDragging || isHovered) ? 80 : 64,
+      decoration: BoxDecoration(
+        color: Colors.primaries[item.icon.hashCode % Colors.primaries.length],
+        shape: BoxShape.circle,
+      ),
+      child: Icon(item.icon, color: Colors.white, size: 32),
+    );
+  }
+}
+
 class DraggableDockItem extends StatefulWidget {
   final DockItem item;
-  final VoidCallback onDelete;
   final int index;
-  final bool isHovered;
+  final VoidCallback onDragStarted;
+  final Function(bool wasAccepted) onDragEnd;
 
-  const DraggableDockItem(
-      {super.key, required this.item,
-      required this.onDelete,
-      required this.index,
-      required this.isHovered});
+  const DraggableDockItem({
+    Key? key,
+    required this.item,
+    required this.onDragStarted,
+    required this.onDragEnd,
+    required this.index,
+  }) : super(key: key);
 
   @override
   _DraggableDockItemState createState() => _DraggableDockItemState();
 }
 
 class _DraggableDockItemState extends State<DraggableDockItem> {
-  Offset offset = Offset.zero;
   bool isDragging = false;
 
   @override
   Widget build(BuildContext context) {
-    return LongPressDraggable<int>(
-      data: widget.index,
+    return Draggable<DockItem>(
+      data: widget.item,
+      dragAnchorStrategy: pointerDragAnchorStrategy,
       feedback: Material(
         color: Colors.transparent,
-        child: Transform.translate(
-          offset: offset,
-          child: buildDockIcon(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 4.0),
+          child: DockIcon(
+            item: widget.item,
+            isHovered: false,
+            isDragging: true,
+          ),
         ),
       ),
-      childWhenDragging: Opacity(
-        opacity: 0.5,
-        child: SizedBox(
-          width: 64,
-          height: 64,
-          child: buildDockIcon(),
-        ),
-      ),
-      onDragEnd: (details) {
-        if (offset.dy < -100) {
-          widget.onDelete();
-        }
-        setState(() {
-          offset = Offset.zero;
-          isDragging = false;
-        });
-      },
+      childWhenDragging: const SizedBox.shrink(),
       onDragStarted: () {
+        widget.onDragStarted();
         setState(() {
           isDragging = true;
         });
       },
-      onDragUpdate: (details) {
+      onDragEnd: (details) {
+        widget.onDragEnd(details.wasAccepted);
         setState(() {
-          offset += details.delta;
+          isDragging = false;
         });
       },
-      child: Transform.translate(
-        offset: offset,
-        child: buildDockIcon(),
+      child: DockIcon(
+        item: widget.item,
+        isHovered: isDragging,
+        isDragging: isDragging,
       ),
-    );
-  }
-
-  Widget buildDockIcon() {
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 200),
-      width: (isDragging || widget.isHovered)
-          ? 80
-          : 64, // Enlarge when dragging or hovered
-      height: (isDragging || widget.isHovered) ? 80 : 64,
-      decoration: BoxDecoration(
-        color: Colors
-            .primaries[widget.item.icon.hashCode % Colors.primaries.length],
-        shape: BoxShape.circle,
-      ),
-      child: Icon(widget.item.icon, color: Colors.white, size: 32),
     );
   }
 }
